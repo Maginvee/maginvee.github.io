1d6e53d16058ef7c6006f735068b8c2516d28b80
Maginveh <maginveh@gmail.com>
2026-05-07 19:49:55 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -130,3 +130,11 @@ $(window).scroll(function () {
 $(document.links).filter(function () {
     return this.hostname != window.location.hostname;
 }).attr('target', '_blank');
+
+
+// Set the overflow property on the body element to prevent scrolling
+document.body.style.overflow = "hidden";
+// Use a timer to enable scrolling after 5 seconds
+setTimeout(function () {
+    document.body.style.overflow = "auto";
+}, 3000); // 3000 milliseconds = 3 seconds
